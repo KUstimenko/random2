892f75e98a9292f5e2e44cc56cc1f66622fc8cd9
Kostiantyn Ustymenko <106483709+KUstimenko@users.noreply.github.com>
2024-03-08 15:53:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// Исходный список случайных слов
+var originalWords = [
+  "Поет",
+  "Мазепа",
+  "Княжна",
+  "Макогон",
+  "Марія",
+  "Сотник",
+  "Відьма",
+  "Неофіти",
+  "Княжна",
+  "Поема",
+  "Повість",
+  "Вірш",
+  "Ніч",
+  "Псалми",
+  "Єретик",
+];
+
+// Копия массива слов для работы с ним
+var words = originalWords.slice();
+
+function generateRandomWord() {
+  // Проверка, есть ли слова в массиве
+  if (words.length === 0) {
+    document.getElementById("randomWord").innerHTML = "Всі слова використані";
+  } else {
+    // Генерация случайного индекса
+    var randomIndex = Math.floor(Math.random() * words.length);
+
+    // Получение и удаление случайного слова из массива
+    var randomWord = words.splice(randomIndex, 1)[0];
+
+    // Вывод слова на странице
+    document.getElementById("randomWord").innerHTML = randomWord;
+  }
+}
